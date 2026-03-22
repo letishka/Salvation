4405ec4bc90b55007773bfe2ce085e8a04264be2
Rotator228xxxPro <k.rotaru2006ru@gmail.com>
2026-03-22 04:36:22 +0300

    Menu & settings
    Меню: играть, настройки, выход. Настройки: звуки, музыка, режим.
    Также были добавлены звуки нажатия кнопок и фоновая музыка

---
+++ ui/main_menu.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var options_menu_scene = preload("res://ui/OptionsMenu.tscn")
+
+
+func _ready() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	$BackgroundMusic.play()
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/test.tscn")
+
+func _on_options_button_pressed() -> void:
+	var options_instance = options_menu_scene.instantiate()
+	add_child(options_instance)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
